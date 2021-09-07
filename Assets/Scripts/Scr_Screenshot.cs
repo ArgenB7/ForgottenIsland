167e8af190eea214d50c1c1e142f63aaf97dcf29
Charlotte <charlottemgalvin@gmail.com>
2021-09-07 18:17:19 +1000

    Added in Ink and Dialogue System. Check read me for more notes.

---
+++ Assets/Scripts/Scr_Screenshot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Screenshot : MonoBehaviour
+{
+    public new string name = "Screenshot ";
+    public int i = 1;
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            print("space key was pressed");
+            ScreenCapture.CaptureScreenshot(name+i+".png");
+            i++;
+        }
+    }
+}
